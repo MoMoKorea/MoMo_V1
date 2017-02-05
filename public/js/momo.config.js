cedfb80a73a17ed9d6219bab53c0458bdcadd0b0
YongGon <dydrhs23@naver.com>
2017-02-05 16:42:15 +0900

    production

--- public/js/momo.config.js
+++ public/js/momo.config.js
@@ -1,12 +1,1 @@
-/**
- * Created by gon on 2017. 1. 1..
- */
-
-$(function () {
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-        }
-    })
-})
-//# sourceMappingURL=momo.config.js.map
+$(function(){$.ajaxSetup({headers:{"X-CSRF-TOKEN":$('meta[name="_token"]').attr("content")}})});
